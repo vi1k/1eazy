73b66c79cbc85dc011e44ce518cb585dbe89a73c
vi1k <v1k.vl@yandex.ru>
2021-12-31 02:23:54 +0300

    2eaz

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Enter the value:";
-    char value [5];
+#include <stdlib.h>
+int main()
+{
+    std::cout << "Enter the value less than 100:";
+    int value;
     std::cin >> value;
-    for (int i = 0; i < 5; ++i)
+    if (value<100)
     {
-        std::cout << i << " value is:" << value[i] << "\n";
+        int f_value = value/10;
+        int s_value = value%10;
+        std::cout << f_value << " " << s_value;
+    }
+    else
+    {
+        exit(1);
     }
 }
